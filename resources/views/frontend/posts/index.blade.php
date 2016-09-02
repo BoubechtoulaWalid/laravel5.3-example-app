743a8287dcbe6e561f4b36e7e971d15a17f065c7
Paul Moff <moff.dev@gmail.com>
2016-09-02 10:11:21 +0700

    basic posts page

---
+++ resources/views/frontend/posts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main');
+
+@section('content')
+    <h1>Blog</h1>
+    <p>Here will be a list of blog posts.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [
+    'uses' => 'PostsController@index',
+    'as'   => 'posts.index'
+]);
+
+Route::get('/blog', [
+    'uses' => 'PostsController@index',
+    'as'   => 'posts.index'
+]);
